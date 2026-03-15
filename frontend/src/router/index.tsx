540b340daf118f5816796cb4a7ea756d63184c4c
Jaki Riggs <jarquealex0302@gmail.com>
2026-03-15 10:40:47 +0100

    feat: add base pages and update router with real routes

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from '../pages/auth/LoginPage'
+import RegisterPage from '../pages/auth/RegisterPage'
+import DashboardPage from '../pages/dashboard/DashboardPage'
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
